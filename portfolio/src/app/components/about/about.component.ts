66e84e791eca7eda808c891050b1a09d70fda51a
hvt733 <115387551+hvt733@users.noreply.github.com>
2023-08-28 20:50:27 +0700

    xong serve o phần contact

--- portfolio/src/app/components/about/about.component.ts
+++ portfolio/src/app/components/about/about.component.ts
@@ -10,5 +10,13 @@ export class AboutComponent {
 
 }
 
+window.addEventListener("DOMContentLoaded", function() {
+  var button = document.getElementById("button_work");
+  var content = document.getElementById("webbb");
 
-
+  button?.addEventListener("click", function() {
+    if (content) {
+      content.classList.toggle("show");
+    }
+  });
+});
